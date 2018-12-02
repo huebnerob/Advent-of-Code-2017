a682ec87e2573c7cf2659d1ab5261a5625065230
Rob Huebner <huebnerob@users.noreply.github.com>
2018-12-01 19:22:14 -0800

    really important update

--- 4.py
+++ 4.py
@@ -5,7 +5,7 @@ for line in input:
 	words = str(line).split()
 	words = ["".join(sorted(word)) for word in words]
 	if len(set(words)) == len(words):
-		print(" ".join(words) + " is a valid passphrase")
+		print(" ".join(words) + " is a valid passphrase yay")
 		valid += 1
 	else:
 		print(" ".join(words) + " is NOT a valid passphrase")
